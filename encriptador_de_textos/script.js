ee540bae6339d86c387a95e40c993a07803e2ea7
Ivan Israel Valencia Barreda <52473941+IvanTenryu@users.noreply.github.com>
2022-12-21 13:51:11 -0600

    Update script.js

--- encriptador_de_textos/script.js
+++ encriptador_de_textos/script.js
@@ -1,6 +1,21 @@
-function myFunction(){
-  var input = document.getElementById('input');
-  var output = document.getElementById('output');
-  
-  output.value = input.value;   
+/* Para remover caracteres especiales */
+function valid(f) {
+  f.value = f.value.normalize("NFD").replace(/[^a-z \u00D1\u00F1]*/ig,''); 
 }
+
+/* Función para encriptar texto ingresado */
+var textoEncriptado;
+function encriptar() {
+  var textoOriginal = document.getElementById("#input").value;
+  textoEncriptado = window.btoa(textoOriginal);
+  document.getElementById("#output").value = textoEncriptado;
+}
+
+/*
+var textoDesencriptado;
+function desencriptar() {
+  var textoEncriptado = document.getElementById("#input").value;
+  textoDesencriptado = window.btoa(textoEncriptado);
+  document.getElementById("#output").value = textoDesencriptado;
+}
+*/ 
